c196fe27359619d98862f6de6965e789489099b9
Jeong Park <wjdtn1220@gmail.com>
2019-08-19 19:32:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,49 +1,19 @@
 def starts_with_a_vowel?(word)
-word.match(/\A[aeiouAEIOU]/) != nil
+  word.match(/^[aeiouAEIOU]\w+/) ? true : false
 end
-
-starts_with_a_vowel?("!ord")
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/un\w*ing/)
+  text.scan(/\bun\w*ing\b/)
 end
-
-words_starting_with_un_and_ending_with_ing("unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary")
 
 def words_five_letters_long(text)
-text.scan(/\b\w{5}\b/)
+  text.scan(/\b\w{5}\b/)
 end
-
-
-words_five_letters_long("extreme briny crepe parking snaps grouping snafu round dog be fork spoon")
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  #Returns true for text starting with a capital letter and ending with puncutation
-  first_letter = text.scan(/\w+/)[0]
-  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
-    true
-  else  
-    false
-  end
-end
-
-def first_word_capitalized_and_ends_with_punctuation_2?(text)
-  if text.match(/^[A-Z]/) && text.match(/[\.]$/)
-    true
-  else 
-    false
-  end
+  text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
-    true
-  else
-    false
-  end
+  phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 end
-
-valid_number = "(800)IloveNY"
-
-valid_phone_number?(valid_number)
-
